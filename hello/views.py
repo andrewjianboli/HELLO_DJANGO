fb9411e8f3e17272323d5b21a78f99c25d3b8203
Andrew <jianbo.li@gmail.com>
2023-11-21 07:40:48 +0800

    new update

--- hello/views.py
+++ hello/views.py
@@ -2,10 +2,17 @@
 
 # Create your views here.
 
+
 import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
+
+
 
 def home(request):
 #   return HttpResponse ("hello Django!")
@@ -30,6 +37,30 @@ def hello_there(request, name):
         }
     )
     
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
+
+
+# Remove the old home function if you want; it's no longer used
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+
     # now = datetime.now()
     # formatted_now = now.strftime("%A, %d %B, %Y at %X")
     #
